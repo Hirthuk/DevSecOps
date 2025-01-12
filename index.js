dfb5000b1f3c29f9b69aa6febb30540dd96c0c22
Sharankumar <sharankumarp02@gmail.com>
2025-01-12 11:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 function sharan = () => {console.log("Sharan")};
 console.log("Additional line got added");
-// For fetch and merge another line
-//For Merge from anoter branch
-//Additon comment ahead of git remote
-//Git commit
