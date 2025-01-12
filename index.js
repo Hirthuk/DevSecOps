90d59d89ba02786d7b2e37206befbc33e29ace57
Hirthuk <sharankumarp02@gmail.com>
2025-01-12 11:35:02 +0530

    merging conflict

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("Additional line got added");
 // For fetch and merge another line
 //For Merge from anoter branch
 //Recent update from remote Gitub
+//Updating from local and there is another update in remote not pulled
+//
+//
