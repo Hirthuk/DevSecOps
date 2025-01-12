328bf0b4683252357b6d947ce894038116314b50
Hirthuk <sharankumarp02@gmail.com>
2025-01-12 11:43:50 +0530

    conflict

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ function sharan = () => {console.log("Sharan")};
 console.log("Additional line got added");
 // For fetch and merge another line
 //For Merge from anoter branch
+//Additon comment ahead of git remote
